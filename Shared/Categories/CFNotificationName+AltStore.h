fe33036976a43d942f278aded7626f5eb566e3dd
Riley Testut <riley@rileytestut.com>
2020-08-31 13:58:44 -0700

    [AltKit] Replaces dedicated AltKit module with shared files across targets
    Treating AltKit as a full module resulted in more complexity than necessary, when we really just wanted to share some files between different targets. Now we can share individual files across modules as-needed without AltKit overhead.

---
+++ Shared/Categories/CFNotificationName+AltStore.h
@@ -0,0 +1,21 @@
+//
+//  CFNotificationName+AltStore.h
+//  AltKit
+//
+//  Created by Riley Testut on 1/10/20.
+//  Copyright © 2020 Riley Testut. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+extern CFNotificationName const ALTWiredServerConnectionAvailableRequest NS_SWIFT_NAME(wiredServerConnectionAvailableRequest);
+extern CFNotificationName const ALTWiredServerConnectionAvailableResponse NS_SWIFT_NAME(wiredServerConnectionAvailableResponse);
+extern CFNotificationName const ALTWiredServerConnectionStartRequest NS_SWIFT_NAME(wiredServerConnectionStartRequest);
+
+extern CFNotificationName const ALTLocalServerConnectionAvailableRequest NS_SWIFT_NAME(localServerConnectionAvailableRequest);
+extern CFNotificationName const ALTLocalServerConnectionAvailableResponse NS_SWIFT_NAME(localServerConnectionAvailableResponse);
+extern CFNotificationName const ALTLocalServerConnectionStartRequest NS_SWIFT_NAME(localServerConnectionStartRequest);
+
+NS_ASSUME_NONNULL_END
